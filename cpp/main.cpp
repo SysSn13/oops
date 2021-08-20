731e4416b4f93f15d8ab5d67c18dbdf5afc9877d
SysSn13 <sudesh18@iitg.ac.in>
2021-08-20 19:35:45 +0530

    use private access specifier

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 class HumanBeing{
-    public: // access specifier
+private: // private access specifier only accessible within the class
+    int age;
+public: // access specifier
     // available outside the class
     string name;
     void display(){
         cout<<"I am a human being"<<endl;
     }
+    void setAge(int age){
+        this->age = age;
+    }
     void introduce(){
-        cout<<"My name is "<<name<<endl;
+        cout<<"My name is "<<name<<". I'm "<<age<<" year old."<<endl;
     }
     void method1();
 }; // semicolon is required
@@ -30,6 +35,11 @@ int main(){
     person.introduce();
     HumanBeing* personPtr = new HumanBeing(); // object created in heap, dynamically allocated
     personPtr->name = "Bob";
+    
+    // personPtr->age = 18; // error as age is private
+    // use a public method to set the age
+    personPtr->setAge(18);
+
     personPtr->introduce();
 
     personPtr->method1();
