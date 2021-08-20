4eb7a2a37bfe5334ee4ec4d42c7f75e4b1707f79
SysSn13 <sudesh18@iitg.ac.in>
2021-08-20 14:31:47 +0530

    add variables

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,12 +4,18 @@ using namespace std;
 class HumanBeing{
     public: // access specifier
     // available outside the class
+    string name;
     void display(){
         cout<<"I am a human being"<<endl;
+    }
+    void introduce(){
+        cout<<"My name is "<<name<<endl;
     }
 }; // semicolon is required
 
 int main(){
     HumanBeing person;
     person.display(); // call the function using dot operator
+    person.name = "Alice";
+    person.introduce();
 }
